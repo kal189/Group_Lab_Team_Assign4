7bc3f60e7c75b91e2fa0993684f694f6f2496864
Absb-coder <pillai.go@northeastern.edu>
2025-10-19 23:22:35 -0400

    added main file

---
+++ TeamGreeting/src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+       public static void main(String[] args) {
+        
+        Member2.main(args);
+        
+        // Uncomment when done
+        // Member1.main(args);
+        // Member3.main(args);
+        
+    }
+    
+}
